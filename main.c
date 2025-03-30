e83602658ecfdbb4ddcd73049410a584fed8dc7d
Nicholas Wang <7414442+nicholascw@users.noreply.github.com>
2025-03-30 16:40:01 -0500

    update usage text

--- main.c
+++ main.c
@@ -53,8 +53,7 @@ print_usage:
       "modify\n"
       "    it under the terms of the GNU General Public License as published "
       "by\n"
-      "    the Free Software Foundation, either version 3 of the License, or\n"
-      "    (at your option) any later version.\n"
+      "    the Free Software Foundation, either version 3 of the License.\n"
       "\n"
       "    This program is distributed in the hope that it will be useful,\n"
       "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
@@ -64,6 +63,6 @@ print_usage:
       "    You should have received a copy of the GNU General Public License\n"
       "    along with this program.  If not, see "
       "<https://www.gnu.org/licenses/>.\n\n");
-  printf("Usage: %s [HOST:[PORT]]\n", argv[0]);
+  printf("Usage: %s [HOST[:PORT]]\n", argv[0]);
   return 1;
 }
