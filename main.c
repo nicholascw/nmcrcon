aec1ab0d4d3f1671bb485857fb7bd0482ce52017
Nicholas Wang <7414442+nicholascw@users.noreply.github.com>
2025-03-30 10:35:45 -0500

    LICENSE PRINT

--- main.c
+++ main.c
@@ -7,15 +7,7 @@
 #include "shell.h"
 #include "socket.h"
 
-#define MAXDATASIZE 8192
-
 int main(int argc, char *argv[]) {
-  printf(
-      "Nicholas' Minecraft RCON Client (nmcrcon)\n"
-      "Copyright (C) 2025 Nicholas Wang (https://nicholas.wang)\n"
-      "This program comes with ABSOLUTELY NO WARRANTY;\n"
-      "This is free software, and you are welcome to redistribute it under \n"
-      "certain conditions.\n\n");
   char *host = NULL;
   char *port = NULL;
   if (argc > 1) {
@@ -52,6 +44,26 @@ int main(int argc, char *argv[]) {
 print_usage:
   if (host) free(host);
   if (port) free(port);
+  printf(
+      "\n    Nicholas' Minecraft RCON Client (nmcrcon): yet another Minecraft "
+      "RCON client that isn't against humanity and actually works.\n"
+      "    Copyright (C) 2025 Nicholas Wang <me@nicho1as.wang>\n"
+      "\n"
+      "    This program is free software: you can redistribute it and/or "
+      "modify\n"
+      "    it under the terms of the GNU General Public License as published "
+      "by\n"
+      "    the Free Software Foundation, either version 3 of the License, or\n"
+      "    (at your option) any later version.\n"
+      "\n"
+      "    This program is distributed in the hope that it will be useful,\n"
+      "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+      "    GNU General Public License for more details.\n"
+      "\n"
+      "    You should have received a copy of the GNU General Public License\n"
+      "    along with this program.  If not, see "
+      "<https://www.gnu.org/licenses/>.\n\n");
   printf("Usage: %s [HOST:[PORT]]\n", argv[0]);
   return 1;
 }
