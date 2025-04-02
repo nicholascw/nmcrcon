ad0a13f01f8483f1d5ab9ec274527fc955586cd8
Nicholas Wang <7414442+nicholascw@users.noreply.github.com>
2025-04-02 03:53:07 -0500

    remove unnecessary headers from socket.c

--- socket.c
+++ socket.c
@@ -1,21 +1,11 @@
 #include <arpa/inet.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
 #include <netdb.h>
-#include <netinet/in.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/epoll.h>
-#include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-
 ssize_t send_all(int sockfd, void *buffer, size_t length) {
   size_t sent = 0;
   ssize_t ret;
